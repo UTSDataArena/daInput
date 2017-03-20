b343d5d178e5f1a2b69f4f651468d9fb0f42cf07
luke.cole <luke.cole@uts.edu.au>
2017-03-21 10:03:37 +1100

    Update optitrack mocap cursor on_move handler implementation.

--- cursor/mocap/OptiTrackMocapCursor.py
+++ cursor/mocap/OptiTrackMocapCursor.py
@@ -8,21 +8,22 @@ from daInput.cursor.mocap.MocapCursor import MocapCursor
 
 class OptiTrackMocapCursor(MocapCursor):
 
+    MOTION_MULTIPLIER = 0.5   # scaling factor
+
     def __init__(self, id, user_id, geometry, ui_context):
         super(OptiTrackMocapCursor, self).__init__(id, user_id, geometry, ui_context)
 
+        self.current = None
+
     def on_move(self, event):
 
-        if event.getExtraDataItems() >= 2:
+        if self.current:
+            dx = (self.current.x - event.getPosition().x) * OptiTrackMocapCursor.MOTION_MULTIPLIER
+            dy = (self.current.y - event.getPosition().y) * OptiTrackMocapCursor.MOTION_MULTIPLIER
 
-            # TODO: this implementation is a guess - confirm actual behaviour
+            self.translate(dx, -dy)  # invert the y-axis
 
-            current = self.get_position()
-
-            dx = event.getPosition().x - current.x
-            dy = event.getPosition().y - current.y
-
-            self.translate(dx, dy)  # TODO: do we need to invert the y-axis?
+        self.current = Vector2(event.getPosition().x, event.getPosition().y)
 
     def on_button_up(self, event):
         super(OptiTrackMocapCursor, self).on_button_up(event)
