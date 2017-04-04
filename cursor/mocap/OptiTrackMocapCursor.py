704d0900fb43e5ae31339777197bba2978823060
luke.cole <luke.cole@uts.edu.au>
2017-04-04 14:44:08 +1000

    Reset origin on cursor activation.

--- cursor/mocap/OptiTrackMocapCursor.py
+++ cursor/mocap/OptiTrackMocapCursor.py
@@ -11,18 +11,32 @@ class OptiTrackMocapCursor(MocapCursor):
     def __init__(self, id, user_id, geometry, mapping, ui_context):
         super(OptiTrackMocapCursor, self).__init__(id, user_id, geometry, ui_context)
 
+        self.is_active = False
+
         self.mapping = mapping
+        self.origin = Vector3(0, 0, 0)
 
     def on_move(self, event):
         position = event.getPosition()
 
         if self.mapping.in_active_region(position):
 
-            x = self.mapping.map_x(position.x)
+            if not self.is_active:
+                # we re-position the origin to wherever the user happens to be when the cursor enters the active region
+
+                self.origin = Vector3(position.x, position.y, 0)
+                self.set_coordinates(Vector3(OptiTrackMocapCursor.MAX_RANGE_VALUE * 0.5, OptiTrackMocapCursor.MAX_RANGE_VALUE * 0.5, self.coordinates.z))
+
+                self.is_active = True
+
+            x = self.mapping.map_x(position.x - self.origin.x)
             y = self.mapping.map_y(position.y)
-            z = self.mapping.map_z(position.z)
+            z = self.mapping.map_z(position.z - self.origin.z)
 
             self.move(x, y, z)
+
+        else:
+            self.is_active = False
 
     def on_button_up(self, event):
         super(OptiTrackMocapCursor, self).on_button_up(event)
