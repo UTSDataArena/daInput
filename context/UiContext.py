604cd778e8029472e32890a7537c81e14ad3efec
luke.cole <luke.cole@uts.edu.au>
2017-04-07 16:16:10 +1000

    Remove unused code.

--- context/UiContext.py
+++ context/UiContext.py
@@ -9,21 +9,10 @@ class UiContext(object):
 
     DEFAULT_CAMERA_FOV = 34.7
 
-    @staticmethod
-    def calculate_aspect():
-        config = getDisplayConfig()
-        size = getDisplayPixelSize()
-
-        width = float(size[0]) / 2 if config.stereoMode == StereoMode.SideBySide else float(size[0])
-        height = float(size[1]) / 2 if config.stereoMode == StereoMode.TopBottom else float(size[1])
-
-        return width / height
-
     def __init__(self):
         super(UiContext, self).__init__()
 
         self.fov = UiContext.DEFAULT_CAMERA_FOV
-        self.aspect = UiContext.calculate_aspect()
 
         self.pointer = MousePointerCursor('mouse')
         self.cursors = []
