7820c6eee34b8ad2822ff27a1bc507e692bd8142
luke.cole <luke.cole@uts.edu.au>
2017-04-07 16:13:31 +1000

    Recalculate cursor plane height as z-depth changes.

--- cursor/CustomGeometryCursor.py
+++ cursor/CustomGeometryCursor.py
@@ -8,7 +8,11 @@ from daInput.cursor.NormalisedCoordinatesCursor import NormalisedCoordinatesCurs
 
 class CustomGeometryCursor(NormalisedCoordinatesCursor):
 
-    def __init__(self, id, user_id, geometry, ui_context, cursor_plane_width=8.0, cursor_plane_height=None, cursor_plane_depth=8.0, motion_multiplier=1):
+    @staticmethod
+    def frustum_height(fov, distance):
+        return abs(2.0 * distance * math.tan(math.radians(fov * 0.5)))
+
+    def __init__(self, id, user_id, geometry, ui_context, cursor_plane_width=8.0, cursor_plane_depth=8.0, motion_multiplier=1):
         super(CustomGeometryCursor, self).__init__(id)
 
         self.user_id = user_id
@@ -16,9 +20,9 @@ class CustomGeometryCursor(NormalisedCoordinatesCursor):
 
         self.ui_context = ui_context
 
-        self.cursor_plane_height = cursor_plane_height if cursor_plane_height else abs(2.0 * self.geometry.getPosition().z * math.tan(math.radians(self.ui_context.fov * 0.5)))
         self.cursor_plane_width = cursor_plane_width * motion_multiplier
         self.cursor_plane_depth = cursor_plane_depth * motion_multiplier
+        self.cursor_plane_height = CustomGeometryCursor.frustum_height(self.ui_context.fov, self.geometry.getPosition().z)
 
     def get_user_id(self):
         return self.user_id
@@ -54,6 +58,8 @@ class CustomGeometryCursor(NormalisedCoordinatesCursor):
 
         self.set_coordinates(coordinates)
         self.geometry.translate(translation, Space.World)
+
+        self.cursor_plane_height = CustomGeometryCursor.frustum_height(self.ui_context.fov, self.geometry.getPosition().z)
 
     def on_move(self, event):
         raise NotImplementedError
